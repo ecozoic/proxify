ecb6d105315d4af03b3eb9f28142abcab74d97af
mark.mosby <mark.mosby@ignitionone.com>
2016-04-12 20:52:00 -0400

    Settings_Extrapolation: added more checks to trapDefinitions.spec.js

--- test/components/utils/trapDefinitions.spec.js
+++ test/components/utils/trapDefinitions.spec.js
@@ -4,6 +4,7 @@ import { trapDefinitions } from '../../../src/components/utils/trapDefinitions';
 describe('trapDefinitions', function trapDefinitionTestModule() {
   it('should have all available traps defined', function trapDefinitionsTest1() {
     trapDefinitions.should.exist;
+    trapDefinitions.should.be.an.object;
     trapDefinitions.should.have.property('defineProperty', 'object');
     trapDefinitions.should.have.property('deleteProperty', 'key');
     trapDefinitions.should.have.property('get', 'key');
@@ -24,6 +25,10 @@ describe('trapDefinitions', function trapDefinitionTestModule() {
 
     keyTraps.should.exist;
     keyTraps.should.have.length(4);
+    keyTraps.should.include('deleteProperty');
+    keyTraps.should.include('get');
+    keyTraps.should.include('getOwnPropertyDescriptor');
+    keyTraps.should.include('set');
   });
 
   it('should return only traps for objects', function trapDefinitionsTest3() {
@@ -31,6 +36,12 @@ describe('trapDefinitions', function trapDefinitionTestModule() {
 
     objTraps.should.exist;
     objTraps.should.have.length(7);
+    objTraps.should.include('defineProperty');
+    objTraps.should.include('getPrototypeOf');
+    objTraps.should.include('isExtensible');
+    objTraps.should.include('ownKeys');
+    objTraps.should.include('preventExtensions');
+    objTraps.should.include('setPrototypeOf');
   });
 
   it('should return only traps for functions', function trapDefinitionsTest4() {
@@ -38,6 +49,8 @@ describe('trapDefinitions', function trapDefinitionTestModule() {
 
     fnTraps.should.exist;
     fnTraps.should.have.length(2);
+    fnTraps.should.include('apply');
+    fnTraps.should.include('construct');
   });
 
   it('should return all traps for properties and objects', function trapDefinitionsTest5() {
@@ -45,6 +58,16 @@ describe('trapDefinitions', function trapDefinitionTestModule() {
 
     objTraps.should.exist;
     objTraps.should.have.length(11);
+    objTraps.should.include('defineProperty');
+    objTraps.should.include('getPrototypeOf');
+    objTraps.should.include('isExtensible');
+    objTraps.should.include('ownKeys');
+    objTraps.should.include('preventExtensions');
+    objTraps.should.include('setPrototypeOf');
+    objTraps.should.include('deleteProperty');
+    objTraps.should.include('get');
+    objTraps.should.include('getOwnPropertyDescriptor');
+    objTraps.should.include('set');
   });
 
   it('should return all traps for properties and functions', function trapDefinitionsTest5() {
@@ -52,19 +75,37 @@ describe('trapDefinitions', function trapDefinitionTestModule() {
 
     fnTraps.should.exist;
     fnTraps.should.have.length(6);
+    fnTraps.should.include('apply');
+    fnTraps.should.include('construct');
+    fnTraps.should.include('deleteProperty');
+    fnTraps.should.include('get');
+    fnTraps.should.include('getOwnPropertyDescriptor');
+    fnTraps.should.include('set');
   });
 
   it('should return all available traps', function trapDefinitionsTest5() {
-    var fnTraps = trapDefinitions.getTraps('all');
+    var traps = trapDefinitions.getTraps('all');
 
-    fnTraps.should.exist;
-    fnTraps.should.have.length(13);
+    traps.should.exist;
+    traps.should.have.length(13);
+    traps.should.include('defineProperty');
+    traps.should.include('getPrototypeOf');
+    traps.should.include('isExtensible');
+    traps.should.include('ownKeys');
+    traps.should.include('preventExtensions');
+    traps.should.include('setPrototypeOf');
+    traps.should.include('deleteProperty');
+    traps.should.include('get');
+    traps.should.include('getOwnPropertyDescriptor');
+    traps.should.include('set');
+    traps.should.include('apply');
+    traps.should.include('construct');
   });
 
   it('should return an empty array', function trapDefinitionsTest5() {
-    var fnTraps = trapDefinitions.getTraps('');
+    var traps = trapDefinitions.getTraps('');
 
-    fnTraps.should.exist;
-    fnTraps.should.have.length(0);
+    traps.should.exist;
+    traps.should.have.length(0);
   });
 });
